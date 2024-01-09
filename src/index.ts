8ceaf2e636db1871504813ba7099aaf9f71b1f9e
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-01-09 15:04:02 +0600

    initial setup of prisma with express.js

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+import app from "./app";
+
+const prisma = new PrismaClient();
+
+const port = process.env.PORT || 5000;
+
+async function main() {
+  app.listen(port, () => {
+    console.log("server is running at ", port);
+  });
+}
+
+main();
